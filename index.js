889253dc8ec56908130e0be3e1b2572286abd693
Kikorono <kikorono.vr@gmail.com>
2022-03-14 13:18:43 -0500

    1.1.0: Updating to eslint 8 and setting up the full member-ordering rule

--- index.js
+++ index.js
@@ -23,7 +23,30 @@ module.exports = {
                 }
             }
         ],
-        "@typescript-eslint/member-ordering": "error",
+        "@typescript-eslint/member-ordering": [
+            "error", { "default":
+            [
+                // Index signature
+
+                // Fields
+                "static-field", // = ["public-static-field", "protected-static-field", "private-static-field"]
+                "abstract-field", // = ["public-abstract-field", "protected-abstract-field", "private-abstract-field"]
+                "instance-field", // = ["public-instance-field", "protected-instance-field", "private-instance-field"]
+
+                // Constructors
+                "constructor",
+
+                // Methods
+                "static-method", // = ["public-static-method", "protected-static-method", "private-static-method"]
+                "abstract-method", // = ["public-abstract-method", "protected-abstract-method", "private-abstract-method"]
+                "instance-method", // = ["public-instance-method", "protected-instance-method", "private-instance-method"]
+
+                // Getters & Setters
+                [ "static-get", "static-set" ], // = ["public-static-get", "protected-static-get", "private-static-get"],["public-static-set", "protected-static-set", "private-static-set"]
+                [ "abstract-get", "abstract-set" ], // = ["public-abstract-get", "protected-abstract-get", "private-abstract-get"],["public-abstract-set", "protected-abstract-set", "private-abstract-set"]
+                [ "instance-get", "instance-set" ], // = ["public-instance-get", "protected-instance-get", "private-instance-get"],["public-instance-set", "protected-instance-set", "private-instance-set"]
+            ] }
+        ],
         "@typescript-eslint/promise-function-async": "error",
         "@typescript-eslint/quotes": [
             "error",
